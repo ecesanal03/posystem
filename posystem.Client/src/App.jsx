5cc72612cdf0dc66c1502bff112ef8bfca9e1e8d
Abel Joseph <abeljoseph1219@gmail.com>
2025-03-23 18:48:31 -0500

    Added Reports Page

--- posystem.Client/src/App.jsx
+++ posystem.Client/src/App.jsx
@@ -6,6 +6,7 @@ import './App.css'
 import Login from "./Pages/Login";
 import Signup from "./Pages/Signup";
 import Marketplace from "./Pages/Marketplace";
+import ReportsPage from './Pages/Reports';
 
 export default function App() {
   return (
@@ -14,6 +15,7 @@ export default function App() {
         <Route path="/" element={<Marketplace />} />
         <Route path="/login" element={<Login />} />
         <Route path="/signup" element={<Signup />} />
+        <Route path="/reports" element={<ReportsPage />} />
       </Routes>
     </Router>
   );
