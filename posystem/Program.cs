ce199205a5347a4091fc67fdea291704871c2f63
ecesanal03 <ecesanal03@gmail.com>
2025-03-24 14:10:58 -0500

    Add license key for service stack

--- posystem/Program.cs
+++ posystem/Program.cs
@@ -2,7 +2,9 @@ using posystem.ServiceInterface;
 using posystem.ServiceInterface.Services;
 
 var builder = WebApplication.CreateBuilder(args);
-builder.WebHost.UseUrls("http://0.0.0.0:8090");
+
+//builder.WebHost.UseUrls("http://0.0.0.0:8090"); //Comment this out for development
+
 var services = builder.Services;
 
 services.AddServiceStack(typeof(MyServices).Assembly);
@@ -30,5 +32,6 @@ app.UseStaticFiles();
 app.UseServiceStack(new AppHost(), options => {
     options.MapEndpoints();
 });
+
 
 app.Run();
